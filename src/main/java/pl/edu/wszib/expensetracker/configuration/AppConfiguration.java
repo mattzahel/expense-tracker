5e29e9c6fecb34a72422a7c3845dca1f7e00fba4
mattzahel <mateusz.zahel@gmail.com>
2022-02-13 12:16:01 +0100

    Initial commit

---
+++ src/main/java/pl/edu/wszib/expensetracker/configuration/AppConfiguration.java
@@ -0,0 +1,20 @@
+package pl.edu.wszib.expensetracker.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("pl.edu.wszib.expensetracker")
+public class AppConfiguration implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
